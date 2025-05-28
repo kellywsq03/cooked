469c3cf8ef88a9636e8aded2d331ba3e9f48b6a3
kellywsq03 <kellywsq03@gmail.com>
2025-05-29 03:16:02 +0800

    Fix bug with gemini

--- prompt.py
+++ prompt.py
@@ -15,17 +15,18 @@ from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 
-# Initialise StateGraph
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
-
-graph_builder = StateGraph(State)
-
 # Initialise LLM
 load_dotenv("keys.env")
 
 if not os.environ.get("GOOGLE_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
 
 llm = init_chat_model(
     model="gemini-2.0-flash", 
@@ -33,12 +34,13 @@ llm = init_chat_model(
     temperature="0.2"
 )
 
-# Initialise tools
 search_tool = JinaSearch()
 
 @tool
 async def get_article(url: str) -> str:
-    """Visit the webpage using the url. Read the article.
+    """Search results only contain snippets of the recipe. You should read the full recipe.
+    You can do this by visiting the webpage using the url. Read the article. If there are links in
+    the article, you should visit those links using this tool as well.
     Args: 
         url: First operand
     """
@@ -65,19 +67,19 @@ class Recipe(BaseModel):
     ingredients: str = Field(..., description="A detailed description of the required ingredients. Provide the required quantities of each ingredient, in UK measurements such as kg and ml.")
     instructions: str = Field(..., description="Provide detailed instructions that are easy to follow by users regardless of their culinary background. When using heat, specify if the heat is low, medium or high. Illustrate with words how each step should be carried out.")
 
-structured_llm = llm.with_structured_output(Recipe)
+structured_llm = recipe_llm.with_structured_output(Recipe)
 
 @tool
-def create_recipe(state: State) -> str:
+def create_recipe(state: State):
     """
-    Creates a recipe following the users requirements. Utilises detailed recipes found on the web as references for the created recipe.
+    Creates a structured recipe. Utilises detailed recipes found on the web as references for the created recipe.
     """
     system_msg = SystemMessage(
         "You are a helpful assistant that creates a recipe by closely referencing the online recipes provided, "
         "using your creativity whenever necessary to fit user requirements. You will maintain as much detail as possible "
         "regarding how the user should handle the food.  Include weight, time or size quantities as much as possible."
     )
-    full_context = state["messages"].copy() + [system_msg]
+    full_context = state["messages"] + [system_msg]
     response = structured_llm.invoke(full_context)
     print(response)
     return response
@@ -86,53 +88,33 @@ tools = [search_tool, get_article, create_recipe]
 llm_with_tools = llm.bind_tools(tools)
 
 def chatbot(state: State):
-    print(state["messages"])
-    repsonse = llm_with_tools.invoke(state["messages"])
-    print(repsonse)
-    return {"messages": [repsonse]}
+    return {"messages": [llm.invoke(state["messages"])]}
 
-# Add chatbot node
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
-
-# Add tool node
-tool_node = ToolNode(tools=tools)
+tool_node = ToolNode(tools=[tool])
 graph_builder.add_node("tools", tool_node)
 graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
 )
-
+graph_builder.add_edge("tools", "chatbot")
 graph = graph_builder.compile()
-# from PIL import Image
-# try:
-#     with open("graph.png", "wb") as f:
-#         f.write(graph.get_graph().draw_mermaid_png())
-#     img = Image.open("graph.png")
-#     img.show()
-# except Exception:
-#     print("exception")
-#     # This requires some extra dependencies and is optional
-#     pass
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
 
-# def stream_graph_updates(user_input: str):
-#     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-#         for value in event.values():
-#             print("Assistant:", value["messages"][-1].content)
-
-# while True:
-#     try:
-#         user_input = "User: Give me a recipe for a high-protein meal."
-#         if user_input.lower() in ["quit", "exit", "q"]:
-#             print("Goodbye!")
-#             break
-#         print("No error ")
-#         stream_graph_updates(user_input)
-#     except:
-#         # fallback if input() is not available
-#         user_input = "What do you know about LangGraph?"
-#         print("User: " + user_input)
-#         stream_graph_updates(user_input)
-#         break
-user_input = "User: Give me a recipe for a high-protein meal."
-graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
