154dceebf923e02a59e03cd3d338adf16ab7090c
kellywsq03 <kellywsq03@gmail.com>
2025-05-29 02:40:52 +0800

    Complete LangGraph. Error encountered with Gemini.

--- prompt.py
+++ prompt.py
@@ -27,7 +27,6 @@ load_dotenv("keys.env")
 if not os.environ.get("GOOGLE_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
-# Initialise chatbot
 llm = init_chat_model(
     model="gemini-2.0-flash", 
     model_provider="google_genai",
@@ -38,7 +37,7 @@ llm = init_chat_model(
 search_tool = JinaSearch()
 
 @tool
-async def get_article(url: str):
+async def get_article(url: str) -> str:
     """Visit the webpage using the url. Read the article.
     Args: 
         url: First operand
@@ -48,14 +47,49 @@ async def get_article(url: str):
     headers = {
         "Authorization": f"Bearer {jina_key}"
     }
-    await response = requests.get(new_link, headers=headers)
+    response = await requests.get(new_link, headers=headers)
     print(response.text)
+    return response.text
 
-tools = [search_tool, get_article]
+recipe_llm = init_chat_model(
+    model="gemini-2.0-flash", 
+    model_provider="google_genai",
+    temperature="0.2"
+)
+
+class Recipe(BaseModel):
+    title: str = Field(..., description="The title of the recipe. This title describes briefly what food the user will make by following the recipe.")
+    serving_size: int = Field(..., description="The number of people that the cooked meal can feed.")
+    prep_time: int = Field(..., description="The amount of time, in minutes, required to prepare the ingredients before cooking this meal. This includes but is not limited to time spent peeling, chopping and washing ingredients.")
+    cook_time: int = Field(..., description="The amount of time, in minutes, required to cook this meal. This is the total amount of time to follow the instructions given in the recipe, subtracted by the prep_time")
+    ingredients: str = Field(..., description="A detailed description of the required ingredients. Provide the required quantities of each ingredient, in UK measurements such as kg and ml.")
+    instructions: str = Field(..., description="Provide detailed instructions that are easy to follow by users regardless of their culinary background. When using heat, specify if the heat is low, medium or high. Illustrate with words how each step should be carried out.")
+
+structured_llm = llm.with_structured_output(Recipe)
+
+@tool
+def create_recipe(state: State) -> str:
+    """
+    Creates a recipe following the users requirements. Utilises detailed recipes found on the web as references for the created recipe.
+    """
+    system_msg = SystemMessage(
+        "You are a helpful assistant that creates a recipe by closely referencing the online recipes provided, "
+        "using your creativity whenever necessary to fit user requirements. You will maintain as much detail as possible "
+        "regarding how the user should handle the food.  Include weight, time or size quantities as much as possible."
+    )
+    full_context = state["messages"].copy() + [system_msg]
+    response = structured_llm.invoke(full_context)
+    print(response)
+    return response
+
+tools = [search_tool, get_article, create_recipe]
 llm_with_tools = llm.bind_tools(tools)
 
 def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    print(state["messages"])
+    repsonse = llm_with_tools.invoke(state["messages"])
+    print(repsonse)
+    return {"messages": [repsonse]}
 
 # Add chatbot node
 graph_builder.add_node("chatbot", chatbot)
@@ -68,75 +102,37 @@ graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
 )
-graph_builder.add_edge("tools", "chatbot")
 
-# Add recipe node
-recipe_llm = init_chat_model(
-    model="gemini-2.0-flash", 
-    model_provider="google_genai",
-    temperature="0.2"
-)
+graph = graph_builder.compile()
+# from PIL import Image
+# try:
+#     with open("graph.png", "wb") as f:
+#         f.write(graph.get_graph().draw_mermaid_png())
+#     img = Image.open("graph.png")
+#     img.show()
+# except Exception:
+#     print("exception")
+#     # This requires some extra dependencies and is optional
+#     pass
 
-def recipe_llm(state: State):
-    system_msg = SystemMessage(
-        "You are a helpful assistant that creates a recipe by closely referencing the online recipes provided, "
-        "using your creativity whenever necessary to fit user requirements. You will maintain as much detail as possible "
-        "regarding how the user should handle the food.  Include weight, time or size quantities as much as possible."
-    )
-    state["messages"].add(system_msg)
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+# def stream_graph_updates(user_input: str):
+#     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+#         for value in event.values():
+#             print("Assistant:", value["messages"][-1].content)
 
-messages = [HumanMessage(("Give me a detailed recipe for a high-protein meal. Include weight, time or size quantities"
-                           "whereever possible. Search the web for recipes. Use the search results as close inspiration for your recipe."))]
-ai_message = llm_with_tools.invoke(messages)
-messages.append(ai_message)
-
-class Link(BaseModel):
-    link: str = Field(..., description="The url to a search result.")
-
-for tool_call in ai_message.tool_calls:
-    selected_tool = {"google_search": search_tool}[tool_call["name"].lower()]
-    results = selected_tool.invoke(tool_call["args"]["__arg1"])
-    
-    tool_msg = ToolMessage(tool_call_id=tool_call["id"], content=results)
-    
-    messages.append(tool_msg)
-    messages.append(HumanMessage("Here’s the search results. From these search results, give me only the link to the most relevant search result."))
-
-structured_llm = llm.with_structured_output(Link)
-top_result = structured_llm.invoke(messages)
-print(top_result.link)
-
-jina_key = os.environ.get("JINA_API_KEY")
-new_link = f"https://r.jina.ai/{top_result.link}"
-headers = {
-    "Authorization": f"Bearer {jina_key}"
-}
-response = requests.get(new_link, headers=headers)
-print(response.text)
-messages.append(HumanMessage(response.text))
-messages.append(HumanMessage("Here is the content of the most relevant search result. Extract the link to the most relevant recipe in the article provided."))
-top_result = structured_llm.invoke(messages)
-print(top_result)
-
-new_link = f"https://r.jina.ai/{top_result.link}"
-headers = {
-    "Authorization": f"Bearer {jina_key}"
-}
-response = requests.get(new_link, headers=headers)
-print(response.text)
-
-messages.append(response.text)
-messages.append(HumanMessage("Here is the most relevant recipe found on the web. Use this result as close inspiration for your recipe."))
-
-class Recipe(BaseModel):
-    title: str = Field(..., description="The title of the recipe. This title describes briefly what food the user will make by following the recipe.")
-    serving_size: int = Field(..., description="The number of people that the cooked meal can feed.")
-    prep_time: int = Field(..., description="The amount of time, in minutes, required to prepare the ingredients before cooking this meal. This includes but is not limited to time spent peeling, chopping and washing ingredients.")
-    cook_time: int = Field(..., description="The amount of time, in minutes, required to cook this meal. This is the total amount of time to follow the instructions given in the recipe, subtracted by the prep_time")
-    ingredients: str = Field(..., description="A detailed description of the required ingredients. Provide the required quantities of each ingredient, in UK measurements such as kg and ml.")
-    instructions: str = Field(..., description="Provide detailed instructions that are easy to follow by users regardless of their culinary background. When using heat, specify if the heat is low, medium or high. Illustrate with words how each step should be carried out.")
-
-structured_llm = llm.with_structured_output(Recipe)
-response = structured_llm.invoke(messages)
-print(response)
+# while True:
+#     try:
+#         user_input = "User: Give me a recipe for a high-protein meal."
+#         if user_input.lower() in ["quit", "exit", "q"]:
+#             print("Goodbye!")
+#             break
+#         print("No error ")
+#         stream_graph_updates(user_input)
+#     except:
+#         # fallback if input() is not available
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
+user_input = "User: Give me a recipe for a high-protein meal."
+graph.invoke({"messages": [{"role": "user", "content": user_input}]})
