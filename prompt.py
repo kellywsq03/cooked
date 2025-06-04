451644cc9877ec56a48aa7b05a7e1df8aad675d0
kellywsq03 <kellywsq03@gmail.com>
2025-06-04 17:35:04 +0800

    Add evaluation nodes

--- prompt.py
+++ prompt.py
@@ -9,12 +9,15 @@ from langchain_core.messages import HumanMessage, ToolMessage, SystemMessage
 from langchain_google_community import GoogleSearchAPIWrapper
 from langchain_community.tools import JinaSearch
 from pydantic import BaseModel, Field
-from typing import Annotated
+from typing import Annotated, Literal
 from typing_extensions import TypedDict
-from langgraph.graph import StateGraph, START
+from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.types import Command, interrupt
+
+file = open("output.txt", "w")
 
 # Initialise LLM
 load_dotenv("keys.env")
@@ -24,11 +27,12 @@ if not os.environ.get("GOOGLE_API_KEY"):
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-    ingredients: list
-
+    sufficient_info: str
+    feedback: str
+    satisfactory: str
+    final_recipe: dict
 
 graph_builder = StateGraph(State)
-
 
 llm = init_chat_model(
     model="gemini-2.0-flash", 
@@ -40,12 +44,11 @@ search_tool = JinaSearch()
 
 @tool
 def get_article(url: str) -> str:
-    """Search results only contain snippets of the recipe. You should read the full recipe.
-    You can do this by visiting the webpage using the url. Read the article. If there are links in
-    the article, you should visit those links using this tool as well.
+    """Read the full recipe by visiting the webpage using the url.
     Args: 
-        url: One url to an article you are interested in reading. The url should only contain one 'www.'
+        url: One url to an article you are interested in reading.
     """
+    print("getting article...")
     jina_key = os.environ.get("JINA_API_KEY")
     new_link = f"https://r.jina.ai/{url}"
     headers = {
@@ -53,6 +56,78 @@ def get_article(url: str) -> str:
     }
     response = requests.get(new_link, headers=headers)
     return response.text
+
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human.
+    Args:
+        query: A question you would like to ask the user.
+    """
+    # human_response = interrupt({"query": query})
+    return {"messages": ["I don't have any preference. Anything will do."]}
+
+research_tools = [search_tool, get_article, human_assistance]
+research_llm = llm.bind_tools(research_tools)
+
+def research(state: State):
+    print("Researching... ")
+    system_msg = "You are a helpful assistant that will search the web for recipes using the jina_search tool. " \
+        "Use relevant search queries to search for recipes using the jina_search tool. " \
+        "After using the search tool, you may read the full recipe by visiting the webpage using the url." \
+        "You can read the article using the get_article tool. If there are links in" \
+        "the article, you can visit those links using the get_article tool as well." \
+        "If you need to ask the user questions, use the human_assistance tool."
+    if state.get("feedback"):
+        print("Feedback: " + state["feedback"])
+        result = research_llm.invoke(state["messages"] + [SystemMessage(system_msg), HumanMessage("Take into account this feedback: " + state["feedback"])])
+    else:
+        print("Researching without feedback...")
+        result = research_llm.invoke(state["messages"] + [SystemMessage(system_msg)])
+    print(result)
+    return {"messages": [result]}
+
+class Feedback(BaseModel):
+    grade: Literal["Sufficient", "Insufficient"] = Field(
+        description="Decide if the current recipes are sufficient. The recipes are sufficient if they contain detailed measurements and quantities of ingredients "\
+            "and specific instructions on how to cook the food at each step."
+    )
+    feedback: str = Field(
+        description="If the recipes are insufficient, suggest what search queries to use.",
+    )
+
+def tools_router(state: State):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the recipe evaluator node.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "research tools"
+    return "research evaluator"
+
+def research_evaluator(state: State):
+    print("evaluating...")
+    system_msg = "You are a helpful assistant that will evaluate if the current recipes provide sufficient information to help another " \
+        "large language model to create a recipe that fulfills the user's requirements. "\
+        "The recipe should be detailed, containing measurements and descriptions of the food at each instruction step whereever possible."\
+        "If the other large language model has asked you for preference, provide feedback that you have no preference. Do not suggest that the "\
+        "large language model needs more information from the user."
+    evaluator_llm = llm.with_structured_output(Feedback)
+    result = evaluator_llm.invoke(state["messages"] + [SystemMessage(system_msg)])
+    print(result, file=file)
+    return {"sufficient_info": result.grade,
+            "feedback": result.feedback}
+
+def route_research(state: State):
+    """Route back to research agent or to summarise agent based on the feedback from the evaluator."""
+    print("Routing from evaluator...")
+    print(state["sufficient_info"] + "!")
+    return state["sufficient_info"]
 
 recipe_llm = init_chat_model(
     model="gemini-2.0-flash", 
@@ -66,59 +141,105 @@ class Recipe(BaseModel):
     prep_time: int = Field(..., description="The amount of time, in minutes, required to prepare the ingredients before cooking this meal. This includes but is not limited to time spent peeling, chopping and washing ingredients.")
     cook_time: int = Field(..., description="The amount of time, in minutes, required to cook this meal. This is the total amount of time to follow the instructions given in the recipe, subtracted by the prep_time")
     ingredients: str = Field(..., description="A detailed description of the required ingredients. Provide the required quantities of each ingredient, in UK measurements such as kg and ml.")
-    instructions: str = Field(..., description="Provide detailed instructions that are easy to follow by users regardless of their culinary background. When using heat, specify if the heat is low, medium or high. Illustrate with words how each step should be carried out.")
+    instructions: str = Field(..., description="Provide detailed instructions that are easy to follow by users regardless of their culinary background. When using heat, specify if the heat is low, medium or high. Illustrate with words how each step should be carried out."\
+                              "Each step should begin with an integer followed by a period, indicating the step number.")
+    url: str = Field(..., description="Referenced urls in the creation of the recipe. Concatanate with commas")
 
 structured_llm = recipe_llm.with_structured_output(Recipe)
 
-@tool
-def create_recipe(state: dict):
+def create_recipe(state: State):
     """
     Creates a structured recipe. Utilises detailed recipes found on the web as references for the created recipe.
     Args:
         state: The entire message history stored in a State object.
     """
+    print("creating recipe... ")
     system_msg = SystemMessage(
         "You are a helpful assistant that creates a recipe by closely referencing the online recipes provided, "
         "using your creativity whenever necessary to fit user requirements. You will maintain as much detail as possible "
-        "regarding how the user should handle the food.  Include weight, time or size quantities as much as possible."
+        "regarding how the user should handle the food. Include weight, time or size quantities as much as possible."
     )
-    full_context = state["messages"] + [system_msg]
+    if state.get("satisfactory"):
+        full_context = state["messages"] + [system_msg] + [HumanMessage(f"Take into account this feedback: {state["feedback"]}")]
+    else:
+        full_context = state["messages"] + [system_msg]
     response = structured_llm.invoke(full_context)
-    return {"messages": [response]}
+    print(state["messages"] + [response], file=file)
+    return {"final_recipe": response}
 
-tools = [search_tool, get_article, create_recipe]
-llm_with_tools = llm.bind_tools(tools)
+class RecipeFeedback(BaseModel):
+    grade: Literal["satisfactory", "unsatisfactory"] = Field(
+        description="Decide if the current recipe is satisfactory."
+    )
+    feedback: str = Field(description="Provide feedback on how to create a more satisfactory recipe.")
 
-def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+def recipe_evaluator(state: State):
+    """
+    Evaluates if the recipe
+    1. Fits user's requirements
+    2. Is detailed about the quantities and measurements of ingredients
+    3. Is detailed in the instructions on how to prepare the food.
+    """
+    print("evaluating recipe...")
+    system_msg = SystemMessage(
+        "You are a helpful assistant that evaluates if the current recipe is satisfactory. A recipe is satisfactory if it: "\
+        "1. Fits user's requirements "\
+        "2. Is detailed about the quantities and measurements of ingredients "\
+        "3. Is detailed in the instructions on how to prepare the food."
+    )
+    input = f"The current recipe is {state["final_recipe"]}"
+    evaluator_llm = llm.with_structured_output(RecipeFeedback)
+    response = evaluator_llm.invoke([system_msg, HumanMessage(input)])
+    print(response, file=file)
+    return {"satisfactory": response.grade,
+            "feedback": response.feedback}
 
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
-tool_node = ToolNode(tools=tools)
-graph_builder.add_node("tools", tool_node)
+def creation_router(state: State):
+    """Route back to creation agent or to end based on the feedback from the evaluator."""
+    print("Routing from creation evaluator...")
+    print(state["satisfactory"] + "!")
+    return state["satisfactory"]
+
+graph_builder.add_node("research", research)
+graph_builder.add_edge(START, "research")
+tool_node = ToolNode(tools=research_tools)
+graph_builder.add_node("research tools", tool_node)
+graph_builder.add_node("research evaluator", research_evaluator)
 graph_builder.add_conditional_edges(
-    "chatbot",
-    tools_condition,
+    "research",
+    tools_router,
+    {"research tools": "research tools",
+     "research evaluator": "research evaluator"}
 )
-graph_builder.add_edge("tools", "chatbot")
-memory = MemorySaver()
-graph = graph_builder.compile(checkpointer=memory)
+graph_builder.add_edge("research tools", "research")
+graph_builder.add_node("create recipe",  create_recipe)
+graph_builder.add_conditional_edges("research evaluator", 
+                                    route_research,
+                                    {"Sufficient": "create recipe",
+                                     "Insufficient": "research"})
+graph_builder.add_node("recipe evaluator", recipe_evaluator)
+graph_builder.add_conditional_edges("recipe evaluator",
+                                    creation_router,
+                                    {"satisfactory": END,
+                                     "unsatisfactory": "create recipe"})
+graph_builder.add_edge("create recipe", "recipe evaluator")
+graph = graph_builder.compile()
+
+try:
+    with open("graph.png", "wb") as f:
+        f.write(graph.get_graph().draw_mermaid_png())
+except Exception:
+    print("exception")
+    pass
 def stream_graph_updates(user_input: str):
-    config = {"configurable": {"thread_id": "1"}}
-    graph.update_state(config, {"ingredients": '[500g of chicken breast, 1 head of broccoli, 1 can of chickpeas]'})
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}, config, stream_mode="values"):
-        snapshot = graph.get_state(config)
-        print({k: v for k, v in snapshot.values.items() if k in ("ingredients")})
-        event["messages"][-1].pretty_print()
-        # for value in event.values():
-        #     print("Assistant:", value["messages"][-1].content)  
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        if "messages" in event:
+            # print(event["messages"])
+            event["messages"][-1].pretty_print()
+            # for value in event.values():
+            #     print("Assistant:", value["messages"][-1].content)  
     
 
-recipe = "High protein meal"
+recipe = input("recipe: ")
 user_input = f"Give me the recipe for {recipe}"
-system_msg = "You are a helpful assistant who creates detailed, structured recipes. The user will ask you for a specific recipe. " \
-"Your agentic workflow should utilise all the tools given in this order: search web -> visit url -> read article -> create recipe. " \
-"In the read article step: 1. If the article does not have a detailed recipe, visit relevant urls until you have a detailed recipe. " \
-"2. If the recipe does not contain the user's ingredients, search for more detailed recipes. " \
-"Once the obtained recipe is detailed and contains the user's ingredients, create a recipe using the create recipe tool in the structured output."
-stream_graph_updates(system_msg + user_input)
+stream_graph_updates(user_input)
